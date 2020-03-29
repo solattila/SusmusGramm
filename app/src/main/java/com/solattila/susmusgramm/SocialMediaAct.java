b53db3e922d5d13b08100b742051b7ddcf46b7d9
Attila <solattila@gmail.com>
2020-03-29 13:57:03 +0200

    Tablayout setup

--- app/src/main/java/com/solattila/susmusgramm/SocialMediaAct.java
+++ app/src/main/java/com/solattila/susmusgramm/SocialMediaAct.java
@@ -1,14 +1,43 @@
 package com.solattila.susmusgramm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class SocialMediaAct extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private TabAdapter mTabAdapter;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+
+        setTitle("Let's Rock");
+
+        mToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(mToolbar);
+
+        mViewPager = findViewById(R.id.viewPager);
+
+        mTabAdapter = new TabAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mTabAdapter);
+
+        mTabLayout = findViewById(R.id.tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager,true);
+
+
     }
 }
---
+++ app/src/main/java/com/solattila/susmusgramm/TabAdapter.java
@@ -0,0 +1,60 @@
+package com.solattila.susmusgramm;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+
+    public TabAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+
+            case 0:
+                ProfileTab profileTab = new ProfileTab();
+                return profileTab;
+            case 1:
+                UsersTab usersTab = new UsersTab();
+                return usersTab;
+            case 2:
+                SharePicTab sharePicTab = new SharePicTab();
+                return sharePicTab;
+            default:
+                return null;
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+
+            case 0:
+                return "Profil";
+            case 1:
+                return "Felhasználók";
+            case 2:
+                return "Kép megosztás";
+            default:
+                return null;
+
+        }
+    }
+}
